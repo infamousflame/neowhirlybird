30eebf6fdf152b4f348f252380e1d0f3dd25674f
infamousflame <67448147+infamousflame@users.noreply.github.com>
2024-11-13 10:01:42 +1300

    Overhaul the game over screen

--- src/game_over_widget.py
+++ src/game_over_widget.py
@@ -1,7 +1,23 @@
 """A widget displayed when the game is over."""
 
+from kivy.app import App
+from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 
 
 class GameOverWidget(Widget):
     """A widget displayed when the game is over."""
+
+
+class GameOverLabel(Widget):
+    """A label displayed when the game is over."""
+
+class RetryButton(Widget):
+    """A button displayed when the game is over, triggers a restart."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def on_touch_up(self, touch):
+        if self.collide_point(*touch.pos):
+            App.get_running_app().start()
