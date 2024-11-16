4e334733991236d0ea0956c8ee90a8088336ca1f
infamousflame <67448147+infamousflame@users.noreply.github.com>
2024-11-16 23:26:41 +1300

    Fix file path issues when both frozen and unfrozen

--- src/main.py
+++ src/main.py
@@ -2,7 +2,19 @@
 have disappeared from many Android devices for an unknown reason.
 """
 
+from os import chdir, getcwd
+from pathlib import Path
+from sys import executable
+import sys
+
 from whirlybird_app import WhirlybirdApp
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    old_cwd: str = getcwd()
+    chdir(
+        Path(executable).parent / '_internal'
+        if getattr(sys, 'frozen', False)
+        else Path(__file__).parent.parent
+    )
     WhirlybirdApp().run()
+    chdir(old_cwd)
