2caf74f9b97fbe7123a46e37dc8d2d50b3a743af
infamousflame <67448147+infamousflame@users.noreply.github.com>
2024-11-23 20:51:50 +1300

    Adjust version number for first beta release

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ have disappeared from many Android devices for an unknown reason.
 """
 
 __author__ = 'Krishna Ranchhod'
-__version__ = '0.2.0'
+__version__ = '1.0.0-beta'
 __license__ = 'MIT'
 
 from src import main
